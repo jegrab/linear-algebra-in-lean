29ef5bc749fb04a62bb8072cc8a200f36f4cd6bc
Jonatan Ziegler <jonatan.ziegler@student.kit.edu>
2026-05-29 12:56:44 +0200

    changed group inverse

--- LinearAlgebraInLean/Group.lean
+++ LinearAlgebraInLean/Group.lean
@@ -17,17 +17,21 @@ class Operation (G: Type u) where
 class Neutral (G: Type u) where
   neutral: G
 
+class GroupInv (G: Type u) where
+  groupInv: G -> G
+
 infixl:65 " ◾ " => Operation.op
 notation "𝟙" => Neutral.neutral
+postfix:max "⁻" => GroupInv.groupInv
 
 
 
-class Group (G: Type u) extends Operation G, Inv G, Neutral G where
+class Group (G: Type u) extends Operation G, GroupInv G, Neutral G where
   assoc : ∀ a b c : G , (a ◾ b) ◾ c = a ◾  (b ◾ c)
   neutral_right : ∀ a : G , a ◾ 𝟙 = a
   neutral_left : ∀ a: G , 𝟙 ◾ a = a
-  inverse_right : ∀ a : G, a ◾  a⁻¹ = 𝟙
-  inverse_left : ∀ a : G, a⁻¹ ◾  a = 𝟙
+  inverse_right : ∀ a : G, a ◾ a⁻ = 𝟙
+  inverse_left : ∀ a : G, a⁻ ◾ a = 𝟙
 
 attribute [simp] Group.assoc Group.neutral_right Group.neutral_left Group.inverse_left Group.inverse_right
 
@@ -35,10 +39,10 @@ attribute [simp] Group.assoc Group.neutral_right Group.neutral_left Group.invers
 instance: CoeSort (Group G) (Type) where
   coe _ := G
 
-instance GroupFromRight [Operation G] [Inv G] [Neutral G]
+instance GroupFromRight [Operation G] [GroupInv G] [Neutral G]
 (assoc: ∀ a b c : G , (a ◾ b) ◾ c = a ◾  (b ◾ c))
 (neutral_right: ∀ a : G , a ◾ 𝟙 = a)
-(inverse_right:  ∀ a : G, a ◾  a⁻¹ = 𝟙)
+(inverse_right:  ∀ a : G, a ◾  a⁻ = 𝟙)
 : Group G where
   assoc := assoc
   inverse_right := inverse_right
@@ -48,29 +52,29 @@ instance GroupFromRight [Operation G] [Inv G] [Neutral G]
     apply Eq.symm
     calc a
       _ = a ◾ 𝟙 := by rw [neutral_right]
-      _ = a ◾ a⁻¹ ◾ a⁻¹⁻¹ := by rw [assoc, inverse_right]
-      _ = 𝟙 ◾ a⁻¹⁻¹  := by rw [inverse_right]
+      _ = a ◾ a⁻ ◾ a⁻⁻ := by rw [assoc, inverse_right]
+      _ = 𝟙 ◾ a⁻⁻  := by rw [inverse_right]
       _ = 𝟙 ◾ a := by grind
   inverse_left := by
     intro a
     apply Eq.symm
     calc 𝟙
-      _ = a⁻¹ ◾ a ◾ (a⁻¹ ◾ a)⁻¹ := by rw [inverse_right]
-      _ = a⁻¹ ◾ a ◾ a⁻¹ ◾ a := by grind
-      _ = a⁻¹ ◾ a := by conv => lhs; lhs; rw [assoc, inverse_right, neutral_right]
+      _ = a⁻ ◾ a ◾ (a⁻ ◾ a)⁻ := by rw [inverse_right]
+      _ = a⁻ ◾ a ◾ a⁻ ◾ a := by grind
+      _ = a⁻ ◾ a := by conv => lhs; lhs; rw [assoc, inverse_right, neutral_right]
 
 
 theorem Group.inv_unique [G: Group G] (a: G): unique (fun x => x ◾ a = 𝟙) := by
   unfold unique
   simp
   intro a₁ a₂ h₁ h₂
-  have := calc a⁻¹
-    _ = 𝟙 ◾ a⁻¹ := by simp
-    _ = a₁ ◾ a ◾ a⁻¹ := by simp [h₁]
+  have := calc a⁻
+    _ = 𝟙 ◾ a⁻ := by simp
+    _ = a₁ ◾ a ◾ a⁻ := by simp [h₁]
     _ = a₁ := by simp
-  have := calc a⁻¹
-    _ = 𝟙 ◾ a⁻¹ := by simp
-    _ = a₂ ◾ a ◾ a⁻¹ := by simp [h₂]
+  have := calc a⁻
+    _ = 𝟙 ◾ a⁻ := by simp
+    _ = a₂ ◾ a ◾ a⁻ := by simp [h₂]
     _ = a₂ := by simp
   simp_all
 
@@ -78,20 +82,20 @@ theorem Group.neutral_unique [G: Group G] : unique (fun x => ∀ a:G, x ◾ a = 
   unfold unique
   simp
   intro b₁ b₂ h₁ h₂
-  have := h₁ b₁⁻¹
-  have := h₂ b₂⁻¹
+  have := h₁ b₁⁻
+  have := h₂ b₂⁻
   simp at *
   simp_all
 
 
-@[simp] theorem Group.inv_inv [G: Group G]: ∀ a: G, a⁻¹⁻¹ = a := by
+@[simp] theorem Group.inv_inv [G: Group G]: ∀ a: G, a⁻⁻ = a := by
   intro a
-  have : a ◾ a⁻¹ = 𝟙 := by simp
-  have := Group.inv_unique (a⁻¹) a⁻¹⁻¹ a
+  have : a ◾ a⁻ = 𝟙 := by simp
+  have := Group.inv_unique (a⁻) a⁻⁻ a
   simp_all
-@[simp] theorem Group.inv_neutral [G: Group G]: (𝟙: G)⁻¹ = 𝟙 := by
+@[simp] theorem Group.inv_neutral [G: Group G]: (𝟙: G)⁻ = 𝟙 := by
   have : (𝟙:G) ◾ 𝟙 = 𝟙 := by simp
-  have := Group.inv_unique (𝟙: G) 𝟙 𝟙⁻¹ this (Group.inverse_left _)
+  have := Group.inv_unique (𝟙: G) 𝟙 𝟙⁻ this (Group.inverse_left _)
   apply Eq.symm
   assumption
 
@@ -124,7 +128,7 @@ def closed_nonzero {S: Type} {z: S} (op: operation S) (h: zero_devisor_free op z
 
 theorem Group.mulWithInversesRight [g: Group G] {a b : g}  : ∃! x : G, a ◾ x = b := by
   unfold existsUnique
-  exists a⁻¹ ◾ b
+  exists a⁻ ◾ b
   simp
   apply And.intro
   have := g.inverse_right
@@ -135,16 +139,16 @@ theorem Group.mulWithInversesRight [g: Group G] {a b : g}  : ∃! x : G, a ◾ x
 
 theorem mulWithInversesLeft (g: Group G) {a b : G}  : ∃! x : G, x ◾ a = b := by
   unfold existsUnique
-  exists b ◾ a⁻¹
+  exists b ◾ a⁻
   simp
   intro y h
-  have h := congrArg (. ◾ a⁻¹) h
+  have h := congrArg (. ◾ a⁻) h
   simp at h
   simp_all
 
 
 
-instance SubGroup [G: Group G] (prop: G -> Prop) (inh: Inhabited (Subtype prop)) (closed: ∀ a b : Subtype prop, prop (a ◾ b⁻¹)):
+instance SubGroup [G: Group G] (prop: G -> Prop) (inh: Inhabited (Subtype prop)) (closed: ∀ a b : Subtype prop, prop (a ◾ b⁻)):
 Group (Subtype prop) :=
   let neutral := by
     exists 𝟙
@@ -152,13 +156,13 @@ Group (Subtype prop) :=
     simp_all
   let inv := by
       intro ⟨a, ha⟩
-      exists a⁻¹
+      exists a⁻
       have := closed neutral ⟨a, ha⟩
       simp [neutral] at this
       assumption
   {
     neutral := neutral
-    inv := inv
+    groupInv := inv
     op := by
       intro ⟨a, ha⟩ ⟨b, hb⟩
       exists a ◾ b
@@ -172,7 +176,7 @@ Group (Subtype prop) :=
     inverse_right := by simp [neutral, inv]
   }
 
-instance AbelianSubGroup [G: AbelianGroup G] (prop: G -> Prop) (inh: Inhabited (Subtype prop)) (closed: ∀ a b : Subtype prop, prop (a ◾ b⁻¹)):
+instance AbelianSubGroup [G: AbelianGroup G] (prop: G -> Prop) (inh: Inhabited (Subtype prop)) (closed: ∀ a b : Subtype prop, prop (a ◾ b⁻)):
 AbelianGroup (Subtype prop) := {
   toGroup := SubGroup prop inh closed
   comm := by simp [Operation.op]
@@ -195,15 +199,15 @@ instance : CoeFun (GroupHom G H) (λ_ => (G -> H)) where
 @[simp] theorem GroupHom.neutral (hom: GroupHom G H): hom 𝟙 = 𝟙 := by
   apply Eq.symm
   calc 𝟙
-    _ = hom 𝟙 ◾ (hom 𝟙)⁻¹ := by simp
-      _ = hom (𝟙 ◾ 𝟙) ◾ (hom 𝟙)⁻¹ := by simp
-      _ = hom 𝟙 ◾ hom 𝟙 ◾ (hom 𝟙)⁻¹ := by rw [hom.property]
+    _ = hom 𝟙 ◾ (hom 𝟙)⁻ := by simp
+      _ = hom (𝟙 ◾ 𝟙) ◾ (hom 𝟙)⁻ := by simp
+      _ = hom 𝟙 ◾ hom 𝟙 ◾ (hom 𝟙)⁻ := by rw [hom.property]
       _ = hom 𝟙 := by simp
-@[simp] theorem GroupHom.inv (hom: GroupHom G H) (a: G): hom a⁻¹ = (hom a)⁻¹ := by
-  have := calc hom a⁻¹ ◾ hom a
-    _ = hom (a⁻¹ ◾ a) := by simp [<-hom.property]
+@[simp] theorem GroupHom.inv (hom: GroupHom G H) (a: G): hom a⁻ = (hom a)⁻ := by
+  have := calc hom a⁻ ◾ hom a
+    _ = hom (a⁻ ◾ a) := by simp [<-hom.property]
     _ = 𝟙 := by simp
-  have := Group.inv_unique (hom a) (hom a)⁻¹ (hom a⁻¹)
+  have := Group.inv_unique (hom a) (hom a)⁻ (hom a⁻)
   simp_all
 
 
