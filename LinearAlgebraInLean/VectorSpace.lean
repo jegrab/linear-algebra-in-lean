01a1c328871ad2e216bedb146bad17908127ff64
Jonatan Ziegler <set>
2026-06-10 13:04:50 +0200

    subspace

--- LinearAlgebraInLean/VectorSpace.lean
+++ LinearAlgebraInLean/VectorSpace.lean
@@ -102,4 +102,53 @@ theorem v_is_lincomb_of_others_from_lin_dep [BEq α][LawfulBEq α]
   . rw [List.contains_eq_mem]
     simpa using h2
 
+@[reducible] def SubSpace (pred: V -> Prop)
+  (inh: Inhabited $ Subtype pred)
+  (closed: ∀ (u v: Subtype pred) (μ: F), pred $ μ • u + v)
+  : VectorSpace F (Subtype pred) :=
+  let subgroup := AbelianSubGroup pred inh <| by
+    intro a b
+    have := closed b a (-1)
+    simp [AbelianGroup.comm, smul_minus] at this
+    apply this
+  let smul μ v := by
+      exists μ • v
+      have h := closed v 0 μ
+      simp [OfNat.ofNat, Zero.zero, subgroup] at h
+      have : V.zero = (0: V) := by unfold OfNat.ofNat; unfold Zero.toOfNat0; simp
+      rw [this] at h
+      simp at h
+      assumption
+  {
+    toAbelianGroup := subgroup
+    smul := smul
+    one_mul  := by
+      intro ⟨v, hv⟩
+      simp [HSMul.hSMul, smul]
+      apply V.one_mul
+    s_assoc := by
+      intro μ γ ⟨c, hc⟩
+      simp [HSMul.hSMul, smul]
+      apply V.s_assoc
+    s_distr_left := by
+      intro μ γ ⟨v ,hv⟩
+      simp [HSMul.hSMul, smul, HAdd.hAdd, subgroup, Add.add]
+      apply V.s_distr_left
+    s_distr_right := by
+      intro μ ⟨u, hu⟩ ⟨v ,hv⟩
+      simp [HSMul.hSMul, smul, HAdd.hAdd, subgroup, Add.add]
+      apply V.s_distr_right
+  }
+
+
+@[reducible] def zero_subspace: VectorSpace F $ Subtype $ (. = (0: V)) := by
+  apply SubSpace
+  . apply Inhabited.mk
+    exists 0
+  . intro ⟨u, hu⟩ ⟨v, hv⟩ μ
+    simp [hu, hv]
+
+
+
+
 end VectorSpace
