cb4f7f030a85e2b1a253865a7046080d5bc6172e
Jonatan Ziegler <set>
2026-06-12 18:56:48 +0200

    vectors space of linear maps

--- LinearAlgebraInLean/Util.lean
+++ LinearAlgebraInLean/Util.lean
@@ -5,7 +5,10 @@ macro "∃!" v:ident " : " t:term ", " b:term : term =>
 
 abbrev unique (pred: α -> Prop) := ∀ a b, pred a -> pred b -> a = b
 
-theorem Zero.zero_is_zero [Zero X]: Zero.zero = (0: X) := by
+theorem Zero.zero_is_ofNat [Zero X]: Zero.zero = (0: X) := by
+  simp [OfNat.ofNat]
+
+theorem One.one_is_ofNat [One X]: One.one = (1: X) := by
   simp [OfNat.ofNat]
 
 notation "(" x:10 "//" y ")" => (⟨x, y⟩ : Subtype _)
--- LinearAlgebraInLean/VectorSpace/LinearMap.lean
+++ LinearAlgebraInLean/VectorSpace/LinearMap.lean
@@ -3,6 +3,7 @@ import LinearAlgebraInLean.VectorSpace.Def
 namespace LA
 
 structure LinearMap (V: VectorSpace F V) (W: VectorSpace F W) extends GroupHom V.toGroup W.toGroup where
+  from_def ::
   scalar: ∀ (μ: F) (v: V), hom (μ • v) = μ • hom v
 
 namespace LinearMap
@@ -14,7 +15,7 @@ attribute[simp] LinearMap.scalar
 instance : CoeFun (LinearMap V W) (λ_ => (V -> W)) where
   coe hom := hom.hom
 
-def simple (hom: V → W) (rule: ∀ (v u: V) (μ: F), hom (μ • v + u) = μ • hom v + hom u) : LinearMap V W :=
+abbrev mk (hom: V → W) (rule: ∀ (v u: V) (μ: F), hom (μ • v + u) = μ • hom v + hom u) : LinearMap V W :=
   let group := {
     hom := hom
     add a b := by
@@ -41,104 +42,142 @@ def ext (φ ψ: LinearMap V W): (∀ x, φ x = ψ x) → φ = ψ := by
 
 
 def chain (φ: LinearMap W X) (ψ: LinearMap V W): LinearMap V X := by
-  apply LinearMap.simple (φ ∘ ψ)
+  apply mk (φ ∘ ψ)
   simp
 
-instance: VectorSpace F $ LinearMap V W where
-  add a b := by
-    apply LinearMap.simple $ fun x => a x + b x
-    intros
-    simp
-    ac_nf
-  smul μ a := by
-    apply LinearMap.simple $ fun x => μ • a x
-    simp
-  neg a := by
-    apply LinearMap.simple $ fun x => - a x
-    simp
-  zero := by
-    apply LinearMap.simple $ fun _ => 0
-    simp
+instance FunVectorSpace (X: Type) (Y: VectorSpace F Y): VectorSpace F $ X -> Y where
+  add a b x := a x + b x
+  smul μ a x := μ • a x
+  neg a x :=  - a x
+  zero x :=  0
   assoc := by
-    simp [HAdd.hAdd, simple]
+    intros
     funext
-    simp [Add.add_eq_hAdd]
+    dsimp [HAdd.hAdd]
+    dsimp [Add.add_eq_hAdd]
+    ac_nf
   neutral_left := by
-    intro
-    apply ext
-    simp [HAdd.hAdd,simple]
-    unfold OfNat.ofNat
-    unfold simple
-    unfold Zero.toOfNat0
-    simp
-    rw [Zero.zero_is_zero]
     intros
-    rw [Add.add_eq_hAdd]
-    simp
+    funext
+    dsimp [HAdd.hAdd, OfNat.ofNat]
+    dsimp [Add.add_eq_hAdd]
+    simp [Zero.zero_is_ofNat]
   neutral_right := by
-    intro
-    apply ext
-    simp [HAdd.hAdd,simple]
-    unfold OfNat.ofNat
-    unfold simple
-    unfold Zero.toOfNat0
-    simp
-    rw [Zero.zero_is_zero]
     intros
-    rw [Add.add_eq_hAdd]
-    simp
+    funext
+    dsimp [HAdd.hAdd, OfNat.ofNat]
+    dsimp [Add.add_eq_hAdd]
+    simp [Zero.zero_is_ofNat]
   inverse_left := by
-    intro
-    apply ext
-    simp [HAdd.hAdd,simple]
-    unfold OfNat.ofNat
-    unfold simple
-    unfold Zero.toOfNat0
-    simp
-    rw [Zero.zero_is_zero]
     intros
-    simp [Add.add_eq_hAdd]
+    funext
+    dsimp [HAdd.hAdd, OfNat.ofNat]
+    simp [Add.add_eq_hAdd, Zero.zero_is_ofNat]
   inverse_right := by
-    intro
-    apply ext
-    simp [HAdd.hAdd,simple]
-    unfold OfNat.ofNat
-    unfold simple
-    unfold Zero.toOfNat0
-    simp
-    rw [Zero.zero_is_zero]
     intros
-    simp [Add.add_eq_hAdd]
+    funext
+    dsimp [HAdd.hAdd, OfNat.ofNat]
+    simp [Add.add_eq_hAdd, Zero.zero_is_ofNat]
   comm := by
     intros
-    apply ext
-    simp [HAdd.hAdd,simple]
-    intro
+    funext
+    dsimp [HAdd.hAdd, OfNat.ofNat]
     simp [Add.add_eq_hAdd]
   one_mul := by
     intros
-    apply ext
-    intro
-    simp [HSMul.hSMul, simple]
-    simp [SMul.smul_eq_hSMul]
+    funext
+    dsimp [HSMul.hSMul, OfNat.ofNat]
+    simp [SMul.smul_eq_hSMul, One.one_is_ofNat]
   s_assoc := by
     intros
-    apply ext
-    intros
-    simp [HSMul.hSMul, simple]
+    funext
+    dsimp [HSMul.hSMul]
     simp [SMul.smul_eq_hSMul]
   s_distr_left := by
     intros
-    apply ext
-    intros
-    simp [HSMul.hSMul, HAdd.hAdd, simple]
+    funext
+    simp [HSMul.hSMul, HAdd.hAdd]
     simp [Add.add_eq_hAdd, SMul.smul_eq_hSMul]
   s_distr_right := by
     intros
-    apply ext
-    intros
-    simp [HSMul.hSMul, HAdd.hAdd, simple]
+    funext
+    simp [HSMul.hSMul, HAdd.hAdd]
     simp [Add.add_eq_hAdd, SMul.smul_eq_hSMul]
+
+instance: VectorSpace F $ LinearMap V W :=
+  let := FunVectorSpace V W
+  {
+    add a b := by
+      apply LinearMap.mk $ a.hom + b.hom
+      intros
+      dsimp [HAdd.hAdd, HSMul.hSMul, Add.add, this, SMul.smul]
+      simp [SMul.smul_eq_hSMul, Add.add_eq_hAdd]
+      ac_nf
+    smul μ a := by
+      apply LinearMap.mk $ μ • a.hom
+      dsimp [HAdd.hAdd, HSMul.hSMul, Add.add, this, SMul.smul]
+      simp [SMul.smul_eq_hSMul, Add.add_eq_hAdd]
+    neg a := by
+      apply LinearMap.mk $ fun x => - a x
+      simp
+    zero := by
+      apply LinearMap.mk $ fun _ => 0
+      simp
+    assoc := by
+      intros
+      dsimp [HAdd.hAdd, HSMul.hSMul, Add.add, this, SMul.smul, mk]
+      simp [SMul.smul_eq_hSMul, Add.add_eq_hAdd]
+    neutral_left := by
+      intros
+      apply ext
+      dsimp [HAdd.hAdd, HSMul.hSMul, Add.add, this, SMul.smul, mk, OfNat.ofNat]
+      simp [SMul.smul_eq_hSMul, Add.add_eq_hAdd, Zero.zero_is_ofNat]
+    neutral_right := by
+      intros
+      apply ext
+      dsimp [HAdd.hAdd, HSMul.hSMul, Add.add, this, SMul.smul, mk, OfNat.ofNat]
+      simp [SMul.smul_eq_hSMul, Add.add_eq_hAdd, Zero.zero_is_ofNat]
+    inverse_left := by
+      intros
+      apply ext
+      dsimp [HAdd.hAdd, HSMul.hSMul, Add.add, this, SMul.smul, mk, OfNat.ofNat]
+      simp [SMul.smul_eq_hSMul, Add.add_eq_hAdd, Zero.zero_is_ofNat]
+    inverse_right := by
+      intros
+      apply ext
+      dsimp [HAdd.hAdd, HSMul.hSMul, Add.add, this, SMul.smul, mk, OfNat.ofNat]
+      simp [SMul.smul_eq_hSMul, Add.add_eq_hAdd, Zero.zero_is_ofNat]
+    comm := by
+      intros
+      apply ext
+      dsimp [HAdd.hAdd, HSMul.hSMul, Add.add, this, SMul.smul, mk, OfNat.ofNat]
+      simp [SMul.smul_eq_hSMul, Add.add_eq_hAdd, Zero.zero_is_ofNat]
+    one_mul := by
+      intros
+      apply ext
+      dsimp [HAdd.hAdd, HSMul.hSMul, Add.add, this, SMul.smul, mk, OfNat.ofNat]
+      simp [SMul.smul_eq_hSMul, Add.add_eq_hAdd, Zero.zero_is_ofNat, One.one_is_ofNat]
+    s_assoc := by
+      intros
+      apply ext
+      dsimp [HAdd.hAdd, HSMul.hSMul, Add.add, this, SMul.smul, mk, OfNat.ofNat]
+      simp [SMul.smul_eq_hSMul, Add.add_eq_hAdd, Zero.zero_is_ofNat, One.one_is_ofNat]
+
+    s_distr_left := by
+      intros
+      apply ext
+      dsimp [HAdd.hAdd, HSMul.hSMul, Add.add, this, SMul.smul, mk, OfNat.ofNat]
+      simp [SMul.smul_eq_hSMul, Add.add_eq_hAdd, Zero.zero_is_ofNat, One.one_is_ofNat]
+    s_distr_right := by
+      intros
+      apply ext
+      dsimp [HAdd.hAdd, HSMul.hSMul, Add.add, this, SMul.smul, mk, OfNat.ofNat]
+      simp [SMul.smul_eq_hSMul, Add.add_eq_hAdd, Zero.zero_is_ofNat, One.one_is_ofNat]
+
+  }
+
+
+
 
 
 
